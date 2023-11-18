0e9ff8b53c43f839a826d614a993c5467e802073
virag <konkoly.t.virag@gmail.com>
2023-11-18 18:57:06 +0100

    Add project files.

---
+++ sof_feleves/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sof_feleves.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
