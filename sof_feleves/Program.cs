9ac404aaf55593d077888894d6d16eb541a20220
virag <konkoly.t.virag@gmail.com>
2023-11-21 21:28:03 +0100

    Email confirmation added to registration

--- sof_feleves/Program.cs
+++ sof_feleves/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using sof_feleves.Models;
 using sof_feleves.Repository;
+using sof_feleves.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +17,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddDefaultIdentity<SiteUser>(options =>
 {
-    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedAccount = true;
     options.Password.RequireDigit = false;
     options.Password.RequiredLength = 4;
     options.Password.RequireNonAlphanumeric = false;
@@ -29,6 +31,8 @@ builder.Services.AddAuthentication().AddFacebook(opt =>
     opt.AppId = "301078806230185";
     opt.AppSecret = "b1489b45628dc2eddf6c6a3ef651192e";
 });
+
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 builder.Services.AddControllersWithViews();
 
---
+++ sof_feleves/Services/EmailSender.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net.Mail;
+using System.Net;
+
+namespace sof_feleves.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            using (SmtpClient client = new SmtpClient()
+            {
+                Host = "smtp.office365.com",
+                Port = 587,
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential("hostieapp@gmail.com", "sof_feleves"), // TODO: ENVIRONMENT VARIABLE
+                TargetName = "STARTTLS/smtp.office365.com",
+                EnableSsl = true
+            })
+            {
+                MailMessage message = new MailMessage()
+                {
+                    From = new MailAddress("hostieapp@gmail.com"),
+                    Subject = subject,
+                    IsBodyHtml = true,
+                    Body = htmlMessage,
+                    BodyEncoding = System.Text.Encoding.UTF8,
+                    SubjectEncoding = System.Text.Encoding.UTF8,
+                };
+                message.To.Add(email);
+                try
+                {
+                    client.Send(message);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+
+}
