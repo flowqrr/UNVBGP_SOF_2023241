eaa2aed9fcd5967ce56624338bef612eb0f715fc
AttilaSzucs21 <116016789+AttilaSzucs21@users.noreply.github.com>
2024-01-13 21:16:40 +0100

    Started the Api implementation

---
+++ sof_feleves/Controllers/Api/StatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StatisticsController : ControllerBase
+{
+    [HttpGet("totalUsers")]
+    public IActionResult GetTotalUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("hostsCount")]
+    public IActionResult HostsCount()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("guestsCount")]
+    public IActionResult GuestCount()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("servicesCount")]
+    public IActionResult ServicesCount()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("servicesCount")]
+    public IActionResult ServicesCount()
+    {
+        throw new NotImplementedException();
+    }
+
+}
